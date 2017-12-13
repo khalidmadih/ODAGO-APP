a274d622ac6524cfa033185d82f23f1a27cc59b2
Khalid Madih <madih_khalid@hotmail.com>
2017-12-12 19:11:17 -0700

    code for autocomplete added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,9 @@ $(document).ready(function() {
 
     //Declaring variables
     var departureCity;
+    var departureAirport;
     var arrivalCity;
+    var arrivalAirport;
     var departureDate;
     var departureTime;
 
@@ -27,7 +29,7 @@ $(document).ready(function() {
     $("#search-flight").on("click", function(event) {
         event.preventDefault();
         departureCity = $("#departure-input").val().trim();
-        arrivalCity = $("#arrival-input").val().trim();
+        // arrivalCity = $("#arrival-input").val().trim();
         departureDate = $("#date-input").val().trim();
         departureTime = $("#time-input").val().trim();
 
@@ -61,6 +63,86 @@ $(document).ready(function() {
 
     });
 
+// Auto complete code
 
+    $(function() {
+    $( '.autocomplete' ).each(function () {
+        var apca = new apc('autocomplete', {
+            key : '1ee2b93fe0', 
+            secret : '86b8fa1526010db', // Your API Secret Key: use this if you are not connecting from a web server
+            limit : 7
+        });
+ 
+        var airPort;
+
+        var dataObj = {
+            source: function( request, response ) {
+                // make the request
+                apca.request( request.term );
+ 
+                // this builds each line of the autocomplete
+                itemObj = function (airport, isChild) {
+                    var label;
+                    console.log("11 :" +airport.city);
+                    if (isChild) { // format children labels
+                        label = '&rdsh;' + airport.iata + ' - ' + airport.name;
+                    
+                    } else { // format labels
+                        label = airport.city;
+                        if (airport.state.abbr) {
+                            label += ', ' + airport.state.abbr;
+                        }
+                        label += ', ' + airport.country.name;                            
+                        label += ' (' + airport.iata + ' - ' + airport.name + ')';
+                    }
+                    return {
+                        label: label,
+                        value: airport.iata + ' - ' + airport.name,
+                        code: airport.iata,
+                        city: airport.city
+                    };
+
+                };
+ 
+                // this deals with the successful response data
+                apca.onSuccess = function (data) {
+                    var listAry = [],
+                        thisAirport;
+                    if (data.status) { // success
+                        for (var i = 0, len = data.airports.length; i < len; i++) {
+                            thisAirport = data.airports[i];
+                            listAry.push(itemObj(thisAirport));
+                            if (thisAirport.children) {
+                                for (var j = 0, jLen = thisAirport.children.length; j < jLen; j++) {
+                                    listAry.push(itemObj(thisAirport.children[j], true));
+                                }
+                            }
+                        }
+                        response(listAry);
+                    } else { // no results
+                        response();
+                    }
+                };
+                apca.onError = function (data) {
+                    response();
+                    console.log(data.message);
+                };
+            },
+            select: function( event, ui ) {
+                // do something for click event
+                arrivalCity = ui.item.city;
+                arrivalAirport = ui.item.code;
+
+                console.log(arrivalCity)
+                console.log(arrivalAirport);
+            }
+        }
+ 
+        // this is necessary to allow html entities to display properly in the jqueryUI labels
+        $(this).autocomplete(dataObj).data("ui-autocomplete")._renderItem = function( ul, item) {
+            return $('<li></li>').data('item.autocomplete', item ).html( item.label ).appendTo( ul );               
+        };
+    });
+});
 
 });
